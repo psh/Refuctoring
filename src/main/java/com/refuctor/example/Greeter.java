553397bdeb8f6cdeb5f2b262e81834504bdf3e79
Paul Hawke <paul.hawke@gmail.com>
2013-07-23 21:45:44 -0500

    Added Spring

--- src/main/java/com/refuctor/example/Greeter.java
+++ src/main/java/com/refuctor/example/Greeter.java
@@ -1,5 +1,8 @@
 package com.refuctor.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Greeter {
 
   private GetterGetter gg = new GetterGetter();
@@ -11,6 +14,8 @@ public class Greeter {
   public Getter eg = gg.createGetter(5);
 
   public String getGreeting() {
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
+
     return hg.getStr() + cg.getStr() + sg.getStr() + wg.getStr() + eg.getStr();
   }
 
