8b2ec12b238675161496b8653a893e602c7dfb51
Paul Hawke <paul.hawke@gmail.com>
2013-07-23 22:11:51 -0500

    Finished adding Spring, and ooops, the test that ought to have been there all along.

--- src/main/java/com/refuctor/example/GetterGetter.java
+++ src/main/java/com/refuctor/example/GetterGetter.java
@@ -1,13 +1,17 @@
 package com.refuctor.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class GetterGetter {
   public Getter createGetter(int i) {
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/com/refuctor/example/application-context.xml");
     switch(i) {
-      case 1: return new HGetter();
-      case 2: return new CGetter();
-      case 3: return new SGetter();
-      case 4: return new WGetter();
-      case 5: return new EGetter();
+      case 1: return applicationContext.getBean("HGetter", HGetter.class);
+      case 2: return applicationContext.getBean("CGetter", CGetter.class);
+      case 3: return applicationContext.getBean("SGetter", SGetter.class);
+      case 4: return applicationContext.getBean("WGetter", WGetter.class);
+      case 5: return applicationContext.getBean("EGetter", EGetter.class);
     }
     return null;
   }
--- src/main/java/com/refuctor/example/Greeter.java
+++ src/main/java/com/refuctor/example/Greeter.java
@@ -1,8 +1,5 @@
 package com.refuctor.example;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class Greeter {
 
   private GetterGetter gg = new GetterGetter();
@@ -14,8 +11,6 @@ public class Greeter {
   public Getter eg = gg.createGetter(5);
 
   public String getGreeting() {
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
-
     return hg.getStr() + cg.getStr() + sg.getStr() + wg.getStr() + eg.getStr();
   }
 
